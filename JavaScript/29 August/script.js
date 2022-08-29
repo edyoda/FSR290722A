555fa011547b366e54a64deeddda3d93fdb47ae9
Divyansh Moonat <DivyanshM@edynamic.net>
2022-08-29 21:30:59 +0530

    JS Intro | Variables | Data Types

---
+++ JavaScript/29 August/script.js
@@ -0,0 +1,131 @@
+console.log("Hello this is my First JS Program");
+console.log("Hello Again");
+
+
+var x = 20;
+var y = 10;
+var total = x+y;
+console.log(x);
+console.log(y);
+console.log(total);
+
+
+var price = 100;
+var price2 = 200;
+var price3 = 300;
+
+// var 2price = 200; WRONG VARIABLE NAME
+// var 3price = 200;
+
+var price_1 = 150;
+var price_2 = 250;
+
+var _price = 10;
+var _price1 = 20;
+
+var $price = 30;
+var $price2 = 40;
+
+var price$1 = 100;
+var price$2 = 200;
+
+// var price%1 = 100; WRONG VARIABLE NAME
+
+// var user rank = 1; SPACE IN A VARIABLE NAME IS NOT ALLOWED
+
+var user_rank = 1; // snake case
+var UserRank = 1; // Pascal case
+var userRank = 1; // camel case
+
+// address of the person
+
+// addressOfThePerson
+
+var price4 = 200.123;
+var temperature = -23.4;
+
+console.log(typeof temperature);
+
+var name = 'John';
+var address = "123, ABC Street, NY, 08817, USA";
+var bloodGroup = "B";
+var price5 = "123";
+
+console.log(typeof name);
+console.log(typeof price5);
+
+
+var canUserVote = false;
+var isValid = true; 
+
+console.log(typeof isValid)
+
+
+// var address = 'John';
+var address; // VARIABLE DECLARATION 
+address = "123, ABC Street"; // VARIABLE DEFINITION
+
+
+var myAddress;
+console.log(myAddress);
+
+
+var myBloodGroup = "";
+console.log(myBloodGroup);
+
+
+var user1 = "John";
+var user2 = "Jane";
+var user3 = "Alex";
+
+var userNames = ["John","Jane","Alex"]; // ARRAY
+
+var userName = "John";
+var userAddress = "123, ABC Street";
+var bloodGroup = "O+";
+
+var user = { // OBJECT
+    name: "John",
+    address: "123, ABC Street",
+    bloodGroup: "O+"
+};
+
+var productName = "Mobile";
+var productPrice = 200;
+
+var product = {
+    name: "Mobile",
+    price: 200
+}
+
+
+
+var num1 = 10;
+var num2 = 20;
+
+var sum = num1+num2;
+console.log(sum);
+
+var num3 = 25;
+var num4 = 7;
+
+var diff = num3-num4;
+console.log(diff);
+
+var num5 = 6;
+var num6 = 11;
+var product = num5*num6;
+console.log(product);
+
+
+var num7 = 20;
+var num8 = 10;
+var div = num7/num8;
+console.log(div);
+
+var num9 = 5;
+var num10 = 0;
+
+var div2 = num9/num10;
+console.log(div2);
+console.log(typeof div2);
